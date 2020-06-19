02668c15ce29cfc3c0c14c6d19d68b944848ed33
Aleksandr Ermakov <mrwin3er@yandex.ru>
2020-06-19 12:21:27 +0300

    exam

---
+++ ExamApp.Tests/LionTests.cs
@@ -0,0 +1,33 @@
+using System;
+using Xunit;
+
+namespace ExamApp.Tests
+{
+    public class LionTests
+    {
+        [Fact]
+        public void ZeroParameters()
+        {
+            Lion lion = new Lion();
+            string location = simba.Location;
+            string boundaries = simba.Boundaries;
+            Assert.Equal("World",location);
+            Assert.Equal("Unknown",boundaries);
+        }
+        public void Create()
+        {
+            Lion simba = new Lion("Африка","Саванны");
+            string location = simba.Location;
+            string boundaries = simba.Boundaries;
+            Assert.Equal("Африка", location);
+            Assert.Equal("Саванны", boundaries);
+        }
+        public void TestMethods()
+        {
+            Lion simba = new Lion("Африка","Саванны");
+            Assert.Equal("Lion is asleep.",simba.Sleep());
+            Assert.Equal("A lion has caught a Zebra and is eating it.",simba.Eat());
+            Assert.Equal("The lion roars.",simba.MakeNoise());
+        }
+    }
+}
---
+++ ExamApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExamApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lion simba = new Lion("Africa","Savannah");
+            Console.WriteLine("Lion:");
+            Console.WriteLine(simba.Sleep());
+            Console.WriteLine(simba.Roam());
+            Console.WriteLine(simba.Eat());
+            Console.WriteLine(simba.MakeNoise());
+
+            Wolf akela = new Wolf("India","Jungle");
+            Console.WriteLine("Wolf:");
+            Console.WriteLine(akela.Sleep());
+            Console.WriteLine(akela.Roam());
+            Console.WriteLine(akela.Eat());
+            Console.WriteLine(akela.MakeNoise());
+        }
+    }
+}
