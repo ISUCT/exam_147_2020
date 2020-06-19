229bab76d47eb6829076fa06f2c8a97d27147359
Aleksandr Ermakov <mrwin3er@yandex.ru>
2020-06-19 12:51:31 +0300

    exam

--- ExamApp.Tests/LionTests.cs
+++ ExamApp.Tests/LionTests.cs
@@ -17,19 +17,31 @@ namespace ExamApp.Tests
         [Fact]
         public void Create()
         {
-            Lion simba = new Lion("Африка","Саванны");
+            Lion simba = new Lion("Africa","Savanna");
             string location = simba.Location;
             string boundaries = simba.Boundaries;
-            Assert.Equal("Африка", location);
-            Assert.Equal("Саванны", boundaries);
+            Assert.Equal("Africa", location);
+            Assert.Equal("Savanna", boundaries);
         }
         [Fact]
         public void TestMethods()
         {
-            Lion simba = new Lion("Африка","Саванны");
+            Lion simba = new Lion("Africa","Savana");
             Assert.Equal("Lion is asleep.",simba.Sleep());
             Assert.Equal("A lion has caught a Zebra and is eating it.",simba.Eat());
             Assert.Equal("The lion roars.",simba.MakeNoise());
+            string location = simba.Location;
+            string boundaries = simba.Boundaries;
+            Assert.Equal($"Lion roams - {location},{boundaries}.",simba.Roam());
+        }
+        [Fact]
+        public void TestNull()
+        {
+            Lion simba = new Lion("Africa","Savana");
+            simba.Boundaries = null;
+            simba.Location = null;
+            Assert.Equal("Africa",simba.Location);
+            Assert.Equal("Savana",simba.Boundaries);
         }
     }
 }
